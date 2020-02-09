60ccd2b14df6c0ae5df8a53abe4ac4b34b20ff8e
yuantongkang <yuantongkang@gmail.com>
2020-02-09 08:42:07 +0800

    驼峰

--- .history/web-ui/src/index_20200209083305.js
+++ .history/web-ui/src/index_20200209083305.js
@@ -1,41 +1,6 @@
 import "./index.css";
 import { jsonToSchema } from "../../lib";
 
-const toCamel = (s) => {
-  return s.replace(/([-_][a-z])/ig, ($1) => {
-    return $1.toUpperCase()
-      .replace('-', '')
-      .replace('_', '');
-  });
-};
-
-const isArray = function (a) {
-  return Array.isArray(a);
-};
-
-const isObject = function (o) {
-  return o === Object(o) && !isArray(o) && typeof o !== 'function';
-};
-
-const keysToCamel = function (o) {
-  if (isObject(o)) {
-    const n = {};
-
-    Object.keys(o)
-      .forEach((k) => {
-        n[toCamel(k)] = keysToCamel(o[k]);
-      });
-
-    return n;
-  } else if (isArray(o)) {
-    return o.map((i) => {
-      return keysToCamel(i);
-    });
-  }
-
-  return o;
-};
-
 const setup = () => {
   const jsonTextarea = document.getElementById("json");
   const graphqlTextarea = document.getElementById("graphql");
@@ -48,7 +13,7 @@ const setup = () => {
     }
 
     try {
-      const jsonObject = keysToCamel(JSON.parse(jsonTextarea.value));
+      const jsonObject = JSON.parse(jsonTextarea.value);
       jsonTextarea.value = JSON.stringify(jsonObject, null, jsonSpaces);
       jsonTextarea.classList.remove("invalid");
 
